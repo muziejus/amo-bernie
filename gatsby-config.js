f0b57429719b30e914f15701f31513a96e8b3a02
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-02-26 14:50:05 -0500

    feat: update config to include better description.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `عمه برني`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Amo Bernie US`,
+    description: `Support Amo Bernie in his campaign for President with Amo Bernie swag.`,
+    author: `@AmoBernieUs`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
