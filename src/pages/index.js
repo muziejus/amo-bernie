dffeccf081374186b2fdbf9f304cd48fe1ac89c9
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-02-26 15:25:12 -0500

    feat: clean up index.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
  <Layout>
     <SEO title="Home" />
-    <div className="p-5 bg-white">
-      <h1 className="text-2xl m-0">Welcome to Amo Bernie US!</h1>
+    <div 
+      className="p-5 bg-abpink-100 opacity-90" 
+    >
+      <h1 className="text-2xl m-0">Welcome!</h1>
       <p className="text-l mb-0">Buttons, sweaters, t-shirts, canvas totes, and stickers to come soon. In the meantime, follow us on Twitter to stay informed.</p>
     </div>
   </Layout>
