af60781b0fda3373aa1f771ab961dffbbd0fcf86
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-03-03 13:59:48 -0500

    Split not me us between the main div.

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,9 +14,17 @@ const AboutPage = () => (
       <p>We’re specifically impressed with the outreach he has done to those who have been especially vilified during the past few administrations: immigrants and Muslims.</p>
       <p>Hence, we’ve adopted Rashida Tlaib’s sobriquet for Senator Sanders, “Amo Bernie,” as our calling card.</p>
       <p>The large majority of proceeds earned on this site will be donated to the Sanders campaign and Representative Tlaib and Representative Ilhan Omar’s reelection campaigns. Similarly, proceeds will also go to organizations fighting for immigrants.</p>
-      <p>Not me. Us.</p>
-      <p>.ليس أنا. الجميع</p>
-      <p className="mb-0">No yo. Nosotros.</p>
+      <div className="flex justify-between flex-row">
+        <div>
+          <p className="mb-0">Not me. Us.</p>
+        </div>
+        <div>
+          <p className="mb-0">.ليس أنا. الجميع</p>
+        </div>
+        <div>
+          <p className="mb-0">No yo. Nosotros.</p>
+        </div>
+      </div>
     </div>
   </Layout>
 )
