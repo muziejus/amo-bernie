87a41f4fbd200da266206c05125a7b9240ec185e
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2020-03-04 15:52:56 -0500

    feat: add max height and overflow to <main>

--- src/pages/bernie.js
+++ src/pages/bernie.js
@@ -10,8 +10,8 @@ const BerniePage = () => (
     >
       <h2 className="text-2xl m-0 text-right">من هو عمو برني؟</h2>
       <p>“Amo Bernie” is how Representative Rashida Tlaib and others have transliterated “عمو برني,” or “uncle Bernie” in Arabic.</p>
-      <p>If you put “Uncle Bernie” into Google Translate, you get “عم بيرني.” This isn’t quite right, but it’s not quite wrong, either. Hopefully not oversimplifying,
-      “عم” is the word for “(paternal) uncle,” and it’s used as a general sign of respect, much like “uncle” is in English. The word features sounds that don’t exist in English, but it’s typically transliterated as “Amm” or “Umm.” On the other hand, no one pronounces “Bernie” like what is suggested by “بيرني.” That reads as “Beernie.” </p>
+      <p>If you <a href="https://translate.google.com/#view=home&op=translate&sl=en&tl=ar&text=uncle%20bernie">put “Uncle Bernie” into Google Translate</a>, you get “عم بيرني.” This isn’t quite right, but it’s not quite wrong, either. Hopefully not oversimplifying,
+      “<a href="https://en.wiktionary.org/wiki/%D8%B9%D9%85#Etymology_3">عم</a>” is the word for “(paternal) uncle,” and it’s used as a general sign of respect, much like “uncle” is in English. The word features sounds that don’t exist in English, but it’s typically transliterated as “Amm” or “Umm.” On the other hand, no one pronounces “Bernie” like what is suggested by “بيرني.” That reads as “Beernie.” </p>
       <p>As for “عمو,” which is to say “Amo” instead of “Amm,” we can start by noting: Arabic is a complicated language. It has a formal, written version that is what you may read in the newspaper or hear on the news. In that case, we would say “My Uncle Bernie” as “عمي برني,” saying “Amee” instead of “Amm.” But Arabic is spoken on every continent on this planet, and local variations exist. One such variation is saying “عمو,” or “Amo,” instead of “Amee.” The expression is more familiar, more everyday, more of the people, and that’s exactly what Amo Bernie would want!</p>
     </div>
   </Layout>
